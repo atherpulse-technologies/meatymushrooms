d3fe7efcc46ba2eca344731f6948df50eb464eda
AmarjithTK <amarjithraveendran@gmail.com>
2025-08-09 22:48:44 +0530

    meatymushrooms sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
+  prefetch: true,
+  site: 'https://meatymushrooms.in', // required for sitemap URLs
+  integrations: [sitemap()],
+  vite: {
     plugins: [tailwindcss()],
   },
 });
